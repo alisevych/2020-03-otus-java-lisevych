e0780d6d1d4f011d2d40410d1475566eb663a6d4
Alena Lisevych <alena.lisevych@firstlinesoftware.com>
2020-06-15 13:41:45 +0300

    hw01: gradle otusJavaLis project, module; HelloGuava factorials; .gitignore updated;

---
+++ hw01-gradle/src/main/java/ru/otus/HelloGuava.java
@@ -0,0 +1,24 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+import com.google.common.math.BigIntegerMath;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloGuava {
+
+    public static void main(String... args) {
+        List<Integer> example = new ArrayList<>();
+        int min = 0;
+        int max = 20;
+        for (int i = min; i < max; i++) {
+            example.add(i);
+        }
+
+        System.out.format("Factorials:\n");
+        for (int item: Lists.reverse(example)) {
+            System.out.format("%5d  -  %20d%n", item, BigIntegerMath.factorial(item));
+        }
+    }
+}
